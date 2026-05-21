938c6c4f5e0b10f0f4d77d55bc1fc57fa8dcaa85
MordenWebDev <mordenwebdeveloper@gmail.com>
2026-05-21 14:27:09 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express =require("express")
+const app=express()
+const PORT=3000
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).json({ message: "server running" })
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running")
+})
