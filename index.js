e77870324d75ec4d944b3823d557917397f26e17
MordenWebDev <mordenwebdeveloper@gmail.com>
2026-05-21 14:55:52 +0530

    folder structure and db connection

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const express =require("express")
 const app=express()
-const PORT=3000
-
+const PORT=process.env.PORT ||3000
+const connectDB=require("./src/config/db")
 
 
 app.get("/",(req,res)=>{
     res.status(200).json({ message: "server running" })
 })
 
+connectDB()
 app.listen(PORT,()=>{
     console.log("server is running")
 })
---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose=require("mongoose")
+const MONGO_URI=process.env.MONGO_URI
+const connectDB=async()=>{
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+module.exports = connectDB;
